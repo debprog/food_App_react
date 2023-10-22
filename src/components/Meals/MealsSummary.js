cfc977349deafcaf0a2e8d20feb71b7e1fc02f7c
debprog <35360322+debprog@users.noreply.github.com>
2023-10-22 18:20:28 +0530

    Add files via upload

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delcious Food Delivered</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et aut illo sint quisquam rerum! Praesentium cupiditate suscipit neque rerum magnam?</p>
+
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et aut illo sint quisquam rerum! Praesentium cupiditate suscipit neque rerum magnam?</p>
+
+        </section>
+    )
+}
+
+export default MealsSummary
